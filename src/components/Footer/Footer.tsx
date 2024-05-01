803712c4e703df3943dc2941116d6833f746d3ef
anmol <mangarajanmol666@gmail.com>
2024-05-01 12:26:16 +0530

    Add new files and configurations for Vite and Tailwind CSS

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+export const Footer = (props: Props) => {
+  return (
+    <div className='bg-slate-900 text-white text-base text-center py-5'>
+
+        Copyright &#169; URLShortner | Anmol Mangaraj
+    </div>
+  )
+}
+
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+interface IHeaderProps {
+}
+
+const Header: React.FunctionComponent<IHeaderProps> = (props) => {
+  return (
+
+<div className='bg-slate-900'>
+<div className="container p-2 mx-auto">
+
+<nav className='py-5'>
+<div className='text-4xl text-white text-center font-extrabold'>LinkForge</div>
+
+</nav>
+
+</div>
+</div>
+
+  );
+};
+
+export default Header;
