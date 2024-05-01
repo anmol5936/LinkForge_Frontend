3833cc0f2c9418da8f642f4e90ac5d69609d246c
anmol <mangarajanmol666@gmail.com>
2024-05-01 19:01:06 +0530

    Add loading spinner to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,37 @@ import * as React from 'react';
 import Header from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
 import Container from './components/Container/Container';
+import { useState, useEffect } from 'react';
 
-interface IAppProps {
-}
+interface IAppProps {}
 
 const App: React.FunctionComponent<IAppProps> = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []); 
+
+  
+
   return (
-<>
-
-<Header/>
-<Container/>
-<Footer/>
-</>
-
-
+    <>
+      {loading ? (
+        <div className="spinner filter contrast-150">
+          <img src="https://res.cloudinary.com/dzxgf75bh/image/upload/v1714570038/1494_unlmwm.gif" alt="Loader" />
+        </div>
+      ) : (
+        <>
+          <Header />
+          <Container />
+          <Footer />
+        </>
+      )}
+    </>
   );
 };
 
